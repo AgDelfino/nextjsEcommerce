988c431b10b1f9d0dc612f314559a6c28f981fbc
AgDelfino <agustin.delfino92@gmail.com>
2022-12-29 23:41:18 -0300

    first commit

---
+++ pages/faq.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+const FAQ = () => {
+    return (
+     <div>
+        <h1>FAQ</h1>
+        <Link href='/'>
+            Ir al Home
+        </Link>
+     </div>
+    )
+}
+
+export default FAQ; 
---
+++ pages/store/index.js
@@ -0,0 +1,9 @@
+const Index = () => {
+    return (
+        <div>
+            <h1>STORE</h1>
+        </div>
+    )
+}
+
+export default Index;
